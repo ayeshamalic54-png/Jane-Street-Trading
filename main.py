d556f7c64b59f8c97a65fea8ba03d19a346f824c
ayeshamalic54-png <ayeshamalic54@gmail.com>
2026-08-20 02:56:52 +0500

    Re-enable Turning Point Inflection filter in main.py

--- main.py
+++ main.py
@@ -2604,9 +2604,12 @@ def main():
                 pass_z_buy = (z < -effective_dyn_z) and (z > -z_sl_val)
                 pass_z_sell = (z > effective_dyn_z) and (z < z_sl_val)
                 
-                # Turning Point Inflection Filter: TEMPORARILY DISABLED FOR TESTING 🔴
+                # Turning Point Inflection Filter: ENABLED 🟢
                 pass_turn_buy = True
                 pass_turn_sell = True
+                if kf_pair and len(kf_pair.z_history) >= 3:
+                    pass_turn_buy = is_turning_point_confirmed(kf_pair.z_history, effective_dyn_z, "BUY_SPREAD")
+                    pass_turn_sell = is_turning_point_confirmed(kf_pair.z_history, effective_dyn_z, "SELL_SPREAD")
 
 
 
